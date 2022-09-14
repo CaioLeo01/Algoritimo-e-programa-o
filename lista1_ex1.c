c6ec173fc43fec4a3f955946e9cdb7dad6a7c4bd
CaioLeo01 <caio.m.c.leonardo@gmail.com>
2022-09-14 20:47:50 -0300

    listas1

---
+++ lista1_ex1.c
@@ -0,0 +1,29 @@
+//contrua um algoritimo que leia 
+
+#include<stdio.h> //biblioteca para os comandosw printf e scanf
+
+int main(){//inicio do algoritimo
+   int  horas minutos;
+   printf("Digite a quantidade de hora: ");
+   scanf("%d",&horas);
+   minutos = horas * 60;
+   segundos = minutos * 60;
+   printf("minutos: %d\n",minutos);
+   printf("segundos: %d",sesegundos)
+
+
+}//final do algoritimo
+
+//contrua um algoritimo que leia 
+
+#include<stdio.h> //biblioteca para os comandosw printf e scanf
+
+int main(){//inicio do algoritimo
+   int  horas minutos;
+   printf("Digite a quantidade de hora: ");
+   scanf("%d",&horas);
+   printf("minutos: %d\n",horas * 60);
+   printf("segundos: %d",minutos * 3600);//ne
+
+
+}//final do algoritimo
