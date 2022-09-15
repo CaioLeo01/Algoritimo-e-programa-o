3c6fa928af411c8e588c824b544ad769605fff64
CaioLeo01 <caio.m.c.leonardo@gmail.com>
2022-09-14 21:50:03 -0300

    lista01_ex04

---
+++ lista01_ex04.c
@@ -0,0 +1,22 @@
+//contrua um algoritimo que  a partirda leitura de dois numeros
+//forneca o resto e oquociente  de divisao de primeiro pelo segundo
+
+#include<stdio.h>
+int main(){
+    int n1, n2;
+    printf("Digite o valor de numeros 1: ");
+    scanf("%d", &n1);
+    printf("Digite o valor do numero 2:" );
+    scanf("%d", &n2);
+    quociente = n1 / n2;
+    resto = n1 % n2;
+    prinf("Quociente: %d\resto: %d", quociente, resto);
+
+
+
+
+
+
+
+
+}
