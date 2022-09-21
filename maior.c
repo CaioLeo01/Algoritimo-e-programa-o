7809c5122c85201be6537dc78ca83560848024e8
CaioLeo01 <caio.m.c.leonardo@gmail.com>
2022-09-21 20:33:55 -0300

    lista2ex01

---
+++ maior.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+   int valor1, valor2;
+printf("Digite dois valores:");
+scanf("%d%d" &valor1, &valor2);
+ 
+if(valor1 > valor2)
+printf("o numero 1 (%d) e maior que o numero 2 (%d)\n",valor1,valor2);
+else printf( "o numero 2 (%d) e maior que o numero 1 (%d)\n",valor2, valor1);
+
+
+
+
+
+
+
+}
