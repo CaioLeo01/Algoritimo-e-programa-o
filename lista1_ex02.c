b6f2d2b3986d82a79e75ddbf733c33837d2ec4a1
CaioLeo01 <caio.m.c.leonardo@gmail.com>
2022-09-28 15:58:09 -0300

    Lista01

--- lista1_ex02.c
+++ lista1_ex02.c
@@ -3,7 +3,7 @@
 #define PI 3.14159//Constante
 int main(){
     //calcular a area do circulo->area - 3.14159 - raio²
-float area, raio
+float area, raio;
 printf("Digite o valor do raio: ");
 scanf("%f", &raio);
 area = PI * pow(raio,2);//POW função matematica  da <math.h>
--- lista1_ex1.c
+++ lista1_ex1.c
@@ -1,29 +1,15 @@
-//contrua um algoritimo que leia 
+#include <stdio.h> //biblioteca para os comandosw printf e scanf
 
-#include<stdio.h> //biblioteca para os comandosw printf e scanf
-
-int main(){//inicio do algoritimo
-   int  horas minutos;
+int main(){
+   int  horas, minutos, segundos;
    printf("Digite a quantidade de hora: ");
    scanf("%d",&horas);
    minutos = horas * 60;
    segundos = minutos * 60;
    printf("minutos: %d\n",minutos);
-   printf("segundos: %d",sesegundos)
+   printf("segundos: %d",segundos);
 
 
 }//final do algoritimo
 
 //contrua um algoritimo que leia 
-
-#include<stdio.h> //biblioteca para os comandosw printf e scanf
-
-int main(){//inicio do algoritimo
-   int  horas minutos;
-   printf("Digite a quantidade de hora: ");
-   scanf("%d",&horas);
-   printf("minutos: %d\n",horas * 60);
-   printf("segundos: %d",minutos * 3600);//ne
-
-
-}//final do algoritime
