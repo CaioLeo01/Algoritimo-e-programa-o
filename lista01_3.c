9e0cbba624d1fef163432971fdfa870f37a4d510
CaioLeo01 <caio.m.c.leonardo@gmail.com>
2022-09-28 09:17:42 -0300

    listao1ex03

---
+++ lista01_3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+    float valor1, valor2, valor3, media;
+printf("Digite o primeiro valor numerico");
+scanf("%f",&valor1);
+printf("Digite o segundo valor numerico");
+scanf("%f",&valor2);
+printf("Digite o terceiro valor numerico");
+scanf("%f",&valor3);
+media =(valor1+valor2+valor3)/3;
+printf("\nA media %.2f\n", media);
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
